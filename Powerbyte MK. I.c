d15332e424af1f4be266d8f58a9e144d3f43c88b
wesser407 <55762268+wesser407@users.noreply.github.com>
2019-12-12 15:25:31 -0500

    Add files via upload
    Software first intergration of Pragma. Not much in this version.

---
+++ Powerbyte MK. I.c
@@ -0,0 +1,11 @@
+#pragma config(Sensor, port2,  touchLED,       sensorVexIQ_LED)
+#pragma config(Sensor, port3,  colorDetector,  sensorVexIQ_ColorHue)
+#pragma config(Sensor, port4,  distanceMM,     sensorVEXIQ_Distance)
+#pragma config(Motor,  motor1,          leftMotor,     tmotorVexIQ, openLoop, driveLeft, encoder)
+#pragma config(Motor,  motor6,          rightMotor,    tmotorVexIQ, openLoop, reversed, driveRight, encoder)
+#pragma config(Motor,  motor10,         armMotor,      tmotorVexIQ, openLoop, encoder)
+
+task main()
+{
+
+}
