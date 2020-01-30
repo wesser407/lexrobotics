7df55755e646026a2ea66105d65d7f6be1beb659
NotPhantomX <55161664+NotPhantomX@users.noreply.github.com>
2020-01-30 15:54:59 -0500

    Update Powerbyte MK. I.c

--- Powerbyte MK. I.c
+++ Powerbyte MK. I.c
@@ -1,19 +1,20 @@
 #pragma config(Sensor, port2,  touchLED,       sensorVexIQ_LED)
 #pragma config(Sensor, port6,  colorDetector,  sensorVexIQ_ColorHue)
-#pragma config(Sensor, port4,  distanceMM,     sensorVEXIQ_Distance)
 #pragma config(Motor,  motor1,          leftMotor,     tmotorVexIQ, openLoop, driveLeft, encoder)
 #pragma config(Motor,  motor6,          rightMotor,    tmotorVexIQ, openLoop, reversed, driveRight, encoder)
 #pragma config(Motor,  motor10,         armMotor,      tmotorVexIQ, openLoop, encoder)
 
-task raisearm()
+task gCube()
 {
-	moveMotor(armMotor, 90, degrees, 50);
+	moveMotor(armMotor, 500, degrees, 70);
 }
+
+
 task main()
 {
-	forward(2.5, rotations, 50);
-	if (getColorName(colorDetector) == colorBlue)
-	{
-		startTask(raisearm);
-	}
+	forward(1.6, rotations, 70);
+  startTask(gCube);
+  forward(2.5, rotations, 70);
+  moveMotor(armMotor, -200, degrees, 70);
+  backward(1, rotations, 70);
 }
